0e3652f6f294883dc7051f7a513a12ed62bdf49c
aftaberski <aftaberski@gmail.com>
2017-07-06 22:09:05 -0400

    Working lil go server

---
+++ lil_http_web_server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":8888")
+	fmt.Println("Listening...")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	for {
+		// Wait for a connection.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		response := `HTTP/1.1 200 OK
+
+    Hello, World!
+    `
+
+		fmt.Println(response)
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
+		go func(c net.Conn) {
+			// Echo all incoming data.
+			c.Write([]byte(response))
+			// Shut down the connection.
+			c.Close()
+		}(conn)
+	}
+}
