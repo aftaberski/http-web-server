1f51968c56058373c45ea44bb10262eebc02d3d4
aftaberski <aftaberski@gmail.com>
2017-07-07 22:23:06 -0400

    WIP

--- lil_http_web_server.go
+++ lil_http_web_server.go
@@ -16,7 +16,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer l.Close()
-
+	var counter = 0
 	for {
 		// Wait for a connection.
 		conn, err := l.Accept()
@@ -24,43 +24,55 @@ func main() {
 			log.Fatal(err)
 		}
 
-		scanner := bufio.NewScanner(conn)
-		scanner.Scan()
+		fmt.Println(counter)
+		fmt.Println(conn)
+		counter++
+		go func(c net.Conn) {
+			defer c.Close()
 
-		requestStr := scanner.Text()
-		uri := strings.Split(requestStr, " ")[1]
+			// TODO: pull out parse header code into a function that takes a scanner
+			// Avoid panics by checking explicitly if the scan returned
+			scanner := bufio.NewScanner(conn)
+			scannedBool := scanner.Scan()
 
-		var headers string
-		var body []byte
+			fmt.Println(scannedBool, scanner.Err())
 
-		switch uri {
-		case "/hello":
-			headers = "HTTP/1.1 200 OK\r\n\r\n"
-			body = []byte("Hello, World!")
-		case "/puppy":
-			body, err = ioutil.ReadFile("puppy.jpg")
-			if err != nil {
-				log.Fatal(err)
+			requestStr := scanner.Text()
+			fmt.Println(requestStr)
+			fmt.Println(len(requestStr))
+			uri := strings.Split(requestStr, " ")[1]
+
+			var headers string
+			var body []byte
+
+			switch uri {
+			case "/hello":
+				headers = "HTTP/1.1 200 OK\r\n\r\n"
+				body = []byte("Hello, World!")
+			case "/puppy":
+				body, err = ioutil.ReadFile("puppy.jpg")
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				contentLength := len(body)
+				// TODO make cute;
+				headers = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\nContent-Length: %d\r\nConnection: close\r\n\r\n", contentLength)
+			default:
+				headers = "HTTP/1.1 404 Not Found\r\n\r\n"
+				body = []byte("404 There's nothing here!")
 			}
 
-			contentLength := len(body)
-			// TODO make cute;
-			headers = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\nContent-Length: %d\r\n\r\n", contentLength)
-		default:
-			headers = "HTTP/1.1 404 Not Found\r\n\r\n"
-			body = []byte("404 There's nothing here!")
-		}
+			fmt.Println(headers)
+			// Handle the connection in a new goroutine.
+			// The loop then returns to accepting, so that
+			// multiple connections may be served concurrently.
 
-		fmt.Println(headers)
-		// Handle the connection in a new goroutine.
-		// The loop then returns to accepting, so that
-		// multiple connections may be served concurrently.
-		go func(c net.Conn) {
 			// Write response to connection
 			c.Write([]byte(headers))
 			c.Write(body)
 			// Shut down the connection.
-			c.Close()
 		}(conn)
+		recover()
 	}
 }
