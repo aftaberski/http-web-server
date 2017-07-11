4bd575eab62bb585a8eaecb4c4b23040342e28a5
aftaberski <aftaberski@gmail.com>
2017-07-11 11:49:17 -0400

    Use bytes buffer instead of bytes array

--- http/response.go
+++ http/response.go
@@ -1,6 +1,9 @@
 package http
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 // Response holds an HTTP response
 type Response struct {
@@ -21,24 +24,23 @@ func NewResponse(proto string, statusCode string, statusMessage string, headers 
 	return &Response{protocol, statusCode, statusMessage, headers, body}, nil
 }
 
-// Format reformats a byte struct into a byte array
-// in order to write it to a connection
-// TODO: Refactor! DRY
-func (res *Response) Format() []byte {
-	// Write status line ex. HTTP/1.1 500 Internal Server Error
-	formattedRes := []byte(fmt.Sprintf("%s %s %s\r\n", res.Protocol, res.StatusCode, res.StatusMessage))
+// Bytes formats a Response to be written to a conn
+// Uses bytes package for efficient string concatenation
+// http://herman.asia/efficient-string-concatenation-in-go
+func (res *Response) Bytes() []byte {
 
-	// Write all headers
+	var buffer bytes.Buffer
+	buffer.WriteString(fmt.Sprintf("%s %s %s\r\n", res.Protocol, res.StatusCode, res.StatusMessage))
+
 	for _, header := range res.Headers {
-		formattedRes = append(formattedRes, fmt.Sprintf("%s\r\n", header)...)
+		buffer.WriteString(fmt.Sprintf("%s\r\n", header))
 	}
 
-	// Blank line separating headers from body as specified for HTTP/1.1
-	formattedRes = append(formattedRes, "\r\n"...)
+	buffer.WriteString("\r\n") // blank line between headers and body
 
 	if len(res.Body) > 0 {
-		formattedRes = append(formattedRes, res.Body...)
+		buffer.Write(res.Body)
 	}
 
-	return formattedRes
+	return buffer.Bytes()
 }
