078003088a243a8bc196565f925ba5764f610bb3
aftaberski <aftaberski@gmail.com>
2017-07-11 12:18:33 -0400

    Add puppy image handler

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "./http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"./http"
+)
 
 func helloHandler(request http.Request) (*http.Response, error) {
 	headers := make([]string, 0)
@@ -9,8 +15,20 @@ func helloHandler(request http.Request) (*http.Response, error) {
 	return http.NewResponse("HTTP/1.1", "200", "OK", headers, body)
 }
 
+func puppyHandler(request http.Request) (*http.Response, error) {
+	body, err := ioutil.ReadFile("puppy.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	contentLength := len(body)
+	headers := []string{"Content-Type: image/jpeg", fmt.Sprintf("Content-Length: %d", contentLength)}
+	return http.NewResponse("HTTP/1.1", "200", "OK", headers, body)
+}
+
 func main() {
 	server := http.NewServer()
 	server.AddRoute(http.GET, "/hello", helloHandler)
+	server.AddRoute(http.GET, "/puppy", puppyHandler)
 	server.Run(8888)
 }
